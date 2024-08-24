ece736d41bd6b4e426460b0838176c508b88dea5
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-08-24 20:22:20 +0200

    RPN.hpp created

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,29 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 22:49:38 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/08/22 22:49:39 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/08/24 20:21:59 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <string>
+#include <stack>
+#include <stdexcept>
+
+class RPN
+{
+public:
+    RPN();
+    ~RPN();
+
+    int evaluate(const std::string& expression);
+
+private:
+    std::stack<int> operands;
+
+    int performOperation(char op, int operand1, int operand2);
+};
+
+#endif
